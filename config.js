3a4cf81cbb709dc60228730bf23e9d6b8747b674
Renata Cunha <atamorim@Renatas-MacBook-Pro.local>
2019-11-27 20:04:20 -0500

    fixed some styles, connected to db, updated text

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port: 8889,
+    uname: "root",
+    pword: "root",
+    dbase: "db_portfolio"
+}
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+const sql = require('../utils/sql');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Renata Cunha' });
+  res.render('index', { title: 'Renata Cunha'});
+
+  let query = `SELECT * FROM tbl_projects`;
+
+  sql.query(query, (err, result) => {
+    if (err) { console.log(err); } // something done broke!
+
+    console.log(result); // 
+
+    res.render('index', { projects: result });
+  })
 });
 
 module.exports = router;
