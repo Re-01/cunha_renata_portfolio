fe12af795faf331319f16cd6490ea196ab461e59
Re-01 <r_cunha@fanshaweonline.ca>
2019-12-05 13:52:39 -0500

    uploading database

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    host: "localhost",
-    port: 8889,
-    uname: "root",
-    pword: "root",
-    dbase: "db_portfolio"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    port: 3306,
+    uname: "b6456e33419c53",
+    pword: "b8158493",
+    dbase: "heroku_57d57a7a8c68d5e"
 }
 
 module.exports = config;
